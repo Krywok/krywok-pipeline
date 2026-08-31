7d79d53203d384862a78764d00a6ee3271e66e86
0x1618 <80158535+0x1618@users.noreply.github.com>
2026-08-31 03:36:11 +0200

    feat: export PipelineException and HandlerException

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -3,6 +3,7 @@ from pipeline.battery.unit import BatteryUnit
 from pipeline.handlers.base.cls import BaseHandler
 from pipeline.handlers.base.modifiers import Context, Item
 from pipeline.handlers.base.resources.constants import HandlerMode
+from pipeline.handlers.base.resources.exceptions import HandlerException
 from pipeline.handlers.condition.cls import ConditionHandler
 from pipeline.handlers.condition.registry import Condition
 from pipeline.handlers.condition.resources.constants import ConditionFlag
@@ -12,9 +13,11 @@ from pipeline.handlers.transform.cls import TransformHandler
 from pipeline.handlers.transform.registry import Transform
 from pipeline.pipe.cls import Pipe
 from pipeline.pipeline.cls import Pipeline
+from pipeline.pipeline.resources.exceptions import PipelineException
 
 __all__ = [
     "Pipe", "Pipeline", "Condition", "Match", "Transform", "Battery",
     "BatteryUnit", "BaseHandler", "ConditionHandler", "MatchHandler",
-    "TransformHandler", "Item", "Context", "HandlerMode", "ConditionFlag"
+    "TransformHandler", "Item", "Context", "HandlerMode", "ConditionFlag",
+    "PipelineException", "HandlerException"
 ]
